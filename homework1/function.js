2c102e89ec71de8474358a4efe2d12f7bef457ec
P-uyoung <wooyoung9654@gmail.com>
2020-04-18 16:36:05 +0900

    homework

---
+++ homework1/function.js
@@ -0,0 +1,17 @@
+function addNum(x,y) {
+    console.log(x+y);
+}
+
+addNum(2,3);
+
+var addStr = function(x,y) {
+    console.log(x+y);
+}
+
+addStr("함수", "표현식");
+
+var addBool = (x,y) => {
+    console.log(x+y);
+}
+
+addBool(true, false);
